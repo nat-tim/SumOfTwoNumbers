4b70418786d12a3a4489e5ed5d2a55e72100fa93
nat-tim <n.a.timosh@yandex.ru>
2022-05-10 07:00:58 +0700

    Initial commit

---
+++ src/test/java/StepDefinition/StepsDefinition.java
@@ -0,0 +1,29 @@
+package StepDefinition;
+
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Тогда;
+
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+public class StepsDefinition {
+    int a;
+    int b;
+    int sum;
+    @Когда("^получаем два целых числа$")
+    public void initNumbers( List<String> arg){
+        a = parseInt(arg.get(0));
+        b = parseInt(arg.get(1));
+    }
+    @Тогда("^складываем их$")
+    public void sumOfNum(){
+        sum = a + b;
+    }
+    @И("^выводим результат$")
+    public void printResult(){
+        System.out.println("сумма чисел " + a + " и " + b + " равна " + sum);
+    }
+
+}
